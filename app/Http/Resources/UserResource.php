c847f573ec71d3c1a7e291737d9259795ed1cd43
Jahongir <alimovj727@gmail.com>
2025-03-04 13:26:35 +0500

    Added search functionality to books API

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -7,13 +7,14 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
-    public function toArray(Request $request): array
+    
+    public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'avatar' => new ImageResource($this->whenLoaded('avatar')),
+        ];
     }
 }
