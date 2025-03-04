bc2e2a5071c72c76f1e5ced32205ebfea16ceb5e
Jahongir <alimovj727@gmail.com>
2025-03-04 13:22:26 +0500

    Added password hashing in user registration

--- app/Http/Controllers/AbstractController.php
+++ app/Http/Controllers/AbstractController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class AbstractController extends Controller
 {
-    //
+    public function uploadFile($file, $path = 'uploads')
+    {
+        return $file->store($path, 'public');
+    }
+
+    public function deleteFile($path)
+    {
+        Storage::disk('public')->delete($path);
+    }
 }
