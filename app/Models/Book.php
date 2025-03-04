54b9038bc8b55aec857ce3adb6b1e65fb4bf99bc
Jahongir <alimovj727@gmail.com>
2025-03-04 13:21:25 +0500

    Created UserResource to format API responses

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Book extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = ['title', 'description', 'author_id'];
+
+    public function images(): MorphMany
+    {
+        return $this->morphMany(Image::class, 'imageable');
+    }
 }
